a48ccbc820192e1f3b4efe905bac0e5293b5d61d
pofystyy <pofystyy@gmail.com>
2025-04-01 21:13:10 +0300

    [PR-23132] minor fix

--- lib/passkit/certificates/source.rb
+++ lib/passkit/certificates/source.rb
@@ -1,7 +1,7 @@
 module Passkit
   module Certificates
     class Source
-      delegate :certificate, :key to: :source
+      delegate :certificate, :key, to: :source
 
       def initialize(certificate_id = nil)
         @certificate_id = certificate_id
@@ -9,10 +9,10 @@ module Passkit
 
       def source
         cert_data = if certificate_id
-            Passkit::Certificate.find(certificate_id)
-          else 
-            Passkit::Certificates::NullCertificate.new
-          end
+          Passkit::Certificate.find(certificate_id)
+        else
+          Passkit::Certificates::NullCertificate.new
+        end
 
         OpenSSL::PKCS12.new(File.read(cert_data.certificate), cert_data.secret)
       end
