93d0396c9bae300d750158568522dbd5c50f7226
Bohdan Perekhrest <winter.summer123456@icloud.com>
2025-09-25 13:38:23 +0300

    CR remarks

--- app/controllers/passkit/api/v1/application_controller.rb
+++ app/controllers/passkit/api/v1/application_controller.rb
@@ -1,10 +1,11 @@
 # frozen_string_literal: true
-
 module Passkit
   module Api
     module V1
       class ApplicationController < ActionController::API
         after_action :allow_iframe
+
+        private
 
         # The X-Frame-Options - header that indicate whether or not a browser should be allowed to render a page in a <frame>
         def allow_iframe
